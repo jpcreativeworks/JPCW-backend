c28b2598cbdfc2bfc1adab180e718ae237567305
Jenn <jp.graphics.011@gmail.com>
2025-07-05 11:06:42 -0700

    added a new iframe route to app and created contact-form for iframe

--- app.py
+++ app.py
@@ -32,3 +32,7 @@ def send_email():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use Render's port or fallback to 5000
     app.run(host="0.0.0.0", port=port)
+
+@app.route("/contact-iframe")
+def contact_iframe():
+    return render_template("contact-form.html")
